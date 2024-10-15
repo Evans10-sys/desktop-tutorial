86508bb1141600044111b5e40d174fd9393c369f
Evans10-sys <evansmutk240@gmail.com>
2024-10-15 18:07:50 +0300

    Add files via upload

---
+++ Assg 2.c
@@ -0,0 +1,13 @@
+
+# i n c l u d e < s t d i o . h>
+i n t main ( v oi d ) {
+i n t a , b , c ;
+p r i n t f ( " 5 : " ) ;
+s c a n f ("% d " , &a ) ;
+p r i n t f ( " 7 :
+ : " ) ;
+s c a n f ("% d " , &b ) ;
+c = a + b ;
+p r i n t f ("% d + %d = %d \ n " , a , b , c ) ;
+r e t u r n 0 ;
+}
